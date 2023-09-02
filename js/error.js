c0334e34a94dc21e1a14d9390cd67b7c448ad34e
Pavel <forestagent2004@gmail.com>
2023-09-02 21:56:10 +0300

    upd

--- js/error.js
+++ js/error.js
@@ -1,51 +1,62 @@
 const submitBtn = document.querySelector('#submit');
-// current year
+// текущий год
 const currentYear = new Date().getFullYear();
 
+// собираем все label
+const forDay = document.querySelector('#forDay');
+const forMonth = document.querySelector('#forMonth');
+const forYear = document.querySelector('#forYear');
+
+// запишем поля для вывода ошибок
+const dayError = forDay.querySelector('.descText');
+const monthError = forMonth.querySelector('.descText');
+const yearError = forYear.querySelector('.descText');
+
+// проверка на валидность даты
 function isDate(y, m, d) {
-
     let date = new Date(y, --m, d);
-
     return date.getFullYear() === y && date.getMonth() === m && date.getDate() === d
 }
 
+// что происходит при клике на кнопку рассчета
 submitBtn.addEventListener('click', () => {
+    // собираем все инпуты 
+    const yearInput = forYear.querySelector('#year').value;
+    const monthInput = forMonth.querySelector('#month').value;
+    const dayInput = forDay.querySelector('#day').value;
 
-    const day = document.querySelector('#forDay');
-    const month = document.querySelector('#forMonth');
-    const year = document.querySelector('#forYear');
-
-    const dayInput = parseInt(day.querySelector('#day').value);
-    const monthInput = parseInt(month.querySelector('#month').value);
-    const yearInput = parseInt(year.querySelector('#year').value);
-
-    console.log(dayInput, monthInput, yearInput)
-
-    if (dayInput == '') {
-        day.querySelector('#day').style.cssText += `border: 1px solid var(--LigthRed);`;
-        day.querySelector('#daytext').style.cssText += `color: var(--LigthRed);`;
-        day.querySelector('.descText').style.cssText += `display: block;`
+    if (dayInput === '') {
+        dayError.innerText = 'this field is required';
+        forDay.querySelector('#day').style.cssText += `border: 1px solid var(--LigthRed);`;
+        forDay.querySelector('#daytext').style.cssText += `color: var(--LigthRed);`;
     }
-
-    if (monthInput == '') {
-        month.querySelector('#month').style.cssText += `border: 1px solid var(--LigthRed);`;
-        month.querySelector('#monthtext').style.cssText += `color: var(--LigthRed);`;
-        month.querySelector('.descText').style.cssText += `display: block;`
+    if (monthInput === '') {
+        monthError.innerText = 'this field is required';
+        forMonth.querySelector('#month').style.cssText += `border: 1px solid var(--LigthRed);`;
+        forMonth.querySelector('#monthtext').style.cssText += `color: var(--LigthRed);`;
     }
-
-    if (yearInput == '') {
-        year.querySelector('#year').style.cssText += `border: 1px solid var(--LigthRed);`;
-        year.querySelector('#yeartext').style.cssText += `color: var(--LigthRed);`;
-        year.querySelector('.descText').style.cssText += `display: block;`
+    if (yearInput === '') {
+        yearError.innerText = 'this field is required';
+        forYear.querySelector('#year').style.cssText += `border: 1px solid var(--LigthRed);`;
+        forYear.querySelector('#yeartext').style.cssText += `color: var(--LigthRed);`;
     }
+    else if ( !isDate(yearInput, monthInput, dayInput) ) {
+        dayError.innerText = 'must be a valid date';
 
-    // if (!isDate(yearInput, monthInput, dayInput) || yearInput > currentYear) {
-    //     document.querySelector('#day').style.cssText += `border: 1px solid var(--LigthRed);`;
-    //     document.querySelector('#daytext').style.cssText += `color: var(--LigthRed);`;
-    //     document.querySelector('#month').style.cssText += `border: 1px solid var(--LigthRed);`;
-    //     document.querySelector('#monthtext').style.cssText += `color: var(--LigthRed);`;
-    //     document.querySelector('#year').style.cssText += `border: 1px solid var(--LigthRed);`;
-    //     document.querySelector('#yeartext').style.cssText += `color: var(--LigthRed);`;
-    // }
+        forDay.querySelector('#day').style.cssText += `border: 1px solid var(--LigthRed);`;
+        forMonth.querySelector('#month').style.cssText += `border: 1px solid var(--LigthRed);`;
+        forYear.querySelector('#year').style.cssText += `border: 1px solid var(--LigthRed);`;
 
+        forDay.querySelector('#daytext').style.cssText += `color: var(--LigthRed);`;
+        forMonth.querySelector('#monthtext').style.cssText += `color: var(--LigthRed);`;
+        forYear.querySelector('#yeartext').style.cssText += `color: var(--LigthRed);`;
+    }
 })
+
+// must be a valid day
+// must be a valid month
+// must be in the past
+
+// must be a valid date (whole)
+
+// this field is required
